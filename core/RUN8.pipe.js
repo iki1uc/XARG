4803dac51ab25d2c728d82ff02ad9dfbc6d65402
iki1uc <entertrainer@web.de>
2026-07-01 23:48:35 +0200

    Update RUN8.pipe.js

--- core/RUN8.pipe.js
+++ core/RUN8.pipe.js
@@ -1,1 +1,17 @@
+import { MIR } from "./MIR.js";
+import { RESPO } from "./RESPO.js";
+import { ARG } from "./ARG.js";
+import { DIR } from "./DIR.js";
+import { REAL } from "./REAL.js";
+import { SAT } from "./SAT.js";
 
+export function RUN8_PIPE(input) {
+  const mir = MIR(input);
+  const respo = RESPO(mir);
+  const arg = ARG(respo);
+  const dir = DIR(arg);
+  const real = REAL(dir);
+  const sat = SAT(real);
+
+  return { mir, respo, arg, dir, real, sat };
+}
