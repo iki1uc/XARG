3002286fb8b8b75715ee342d86ce1ac9e8c2540a
iki1uc <entertrainer@web.de>
2026-07-01 22:19:25 +0200

    Update XARG.js

--- core/XARG.js
+++ core/XARG.js
@@ -1,1 +1,17 @@
+export const XARG = {
+  id: "XARG",
+  raw: "RAW#XARG",
+  mass: true,
 
+  bind(mir, respo, arg, dir) {
+    return {
+      id: this.id,
+      raw: this.raw,
+      mass: this.mass,
+      mir,
+      respo,
+      arg,
+      dir
+    };
+  }
+};
