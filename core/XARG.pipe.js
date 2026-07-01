53814a9ddc1bf9e538d999b3f247efa5d3f986d0
iki1uc <entertrainer@web.de>
2026-07-01 22:21:10 +0200

    Update XARG.pipe.js

--- core/XARG.pipe.js
+++ core/XARG.pipe.js
@@ -1,1 +1,23 @@
+import { XARG_MASS } from "./XARG.mass.js";
+import { MIR } from "./MIR.js";
+import { RESPO } from "./RESPO.js";
+import { ARG } from "./ARG.js";
+import { DIR } from "./DIR.js";
+import { REAL } from "./REAL.js";
 
+export function XARG_PIPE(input) {
+  const mir = MIR(input);
+  const respo = RESPO(mir);
+  const arg = ARG(respo);
+  const dir = DIR(arg);
+  const real = REAL(dir);
+
+  return {
+    xarg: XARG_MASS,
+    mir,
+    respo,
+    arg,
+    dir,
+    real
+  };
+}
